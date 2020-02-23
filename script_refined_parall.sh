e2645afad69c74914367bf0cb875ca184adb2bcb
Simone Ammazzalorso <simone.ammazzalorso@unito.it>
2020-02-23 14:05:36 +0100

    minor docker

--- script_refined_parall.sh
+++ script_refined_parall.sh
@@ -3,4 +3,4 @@ source activate tensorflow
 #conda list -n tensorflow
 conda info -e
 
-python /run_CNN/CNN_parallel.py --tag=CNN_notnorm --tag_res=run1_ocprl --path=/archive/home/sammazza/radioML/ --path_TD=/archive/home/sammazza/radioML/data/mapsim_PS/ --path_TL=/archive/home/sammazza/radioML/data/mapsim_PS/ --N_start=0 --N_stop=10000 --N_epochs_small=15 --N_epochs_large=15 --batch_size_small=4 --batch_size_large=4 --train --refined --suffix=x1000
+python /run_CNN/CNN_parallel.py --tag=CNN_notnorm --tag_res=run2_ocprl --path=/archive/home/sammazza/radioML/ --path_TD=/archive/home/sammazza/radioML/data/mapsim_PS/ --path_TL=/archive/home/sammazza/radioML/data/mapsim_PS/ --N_start=0 --N_stop=14999 --N_epochs_small=15 --N_epochs_large=15 --batch_size_small=4 --batch_size_large=4 --train --refined --pool --suffix=x1000
